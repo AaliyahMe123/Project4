4685a13f2df366783e4cdd52f9e0b1dda594d751
Aaliyah Melton <aaliyahmariah09@gmail.com>
2017-10-24 10:03:45 -0400

    Added Bcrypt

--- project4/app/models/user.rb
+++ project4/app/models/user.rb
@@ -17,5 +17,18 @@ class User < ApplicationRecord::Base
   end
   def clear_password
     self.password = nil
-  end 
+  end
+  def self.authentication(username_or_email="", login_password="")
+    if EMAIL_REGEX.match(username_or_email)
+      user = User.find_by_email(username_or_email)
+    else
+      user = User.find_by_username(username_or_email)
+    end
+    if user && user.match_password(login_password)
+      return user
+    else
+      return false
+    end
+  end
+  def match_password
 end
--- project4/config/routes.rb
+++ project4/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/login,'
+
+  get 'sessions/home,'
+
+  get 'sessions/profile,'
+
+  get 'sessions/setting'
+
   get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
